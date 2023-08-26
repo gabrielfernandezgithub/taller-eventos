f9a6d360a3b79cf524f3df3c6f1ee1d16ececd09
gabrielfernandezgithub <gabrielfernandez.esi@gmail.com>
2023-08-26 15:08:38 -0300

    Agregador manejador de eventos al div

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const divContainer = document.querySelector('.boton-container');
+
+    divContainer.addEventListener('click', function() {
+        alert('¡Hola! Soy el div');
+    });
+});
